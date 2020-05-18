ccd8c96a16caf0f024936db66c0a4604bc13af62
Maya Mathur <mmathur@stanford.edu>
2020-05-18 15:51:57 -0700

    changed URL in settings

--- src/cochrane_scraper/settings.clj
+++ src/cochrane_scraper/settings.clj
@@ -1,7 +1,8 @@
 (ns cochrane-scraper.settings)
 
 (def ^:dynamic *cochrane-base* "/doi/10.1002/14651858.CD")
-(def ^:dynamic *wiley-base* "http://onlinelibrary.wiley.com")
+(def ^:dynamic *wiley-base* "https://www.cochranelibrary.com/cdsr")
+;;Previously was: (def ^:dynamic *wiley-base* "http://onlinelibrary.wiley.com")
 (def ^:dynamic *first-cols* [:comparison :outcome :subgroup 
                              :name :study_id :data_type :method
                              :effect_measure :random :totals])
