7435ee27700dadce8d575a2fc6fb4cf9c6b7d52f
Evan <kontopantelis@gmail.com>
2020-05-22 14:45:43 +0100

    Update settings.clj

--- src/cochrane_scraper/settings.clj
+++ src/cochrane_scraper/settings.clj
@@ -1,7 +1,7 @@
 (ns cochrane-scraper.settings)
 
 (def ^:dynamic *cochrane-base* "/doi/10.1002/14651858.CD")
-(def ^:dynamic *wiley-base* "http://onlinelibrary.wiley.com")
+(def ^:dynamic *wiley-base* "http://cochranelibrary-wiley.com")
 (def ^:dynamic *first-cols* [:comparison :outcome :subgroup 
                              :name :study_id :data_type :method
                              :effect_measure :random :totals])
